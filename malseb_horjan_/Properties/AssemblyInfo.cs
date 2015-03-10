4e697b63f3c528e43cb39f1a3ca09516e262c954
Zebezd <zebbom@hotmail.com>
2015-03-10 11:59:25 +0100

    Project settings, get old name out of there.

--- malseb_horjan_/Properties/AssemblyInfo.cs
+++ malseb_horjan_/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("malseb_horjan_")]
+[assembly: AssemblyTitle("PG4500_2015_Innlevering1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("malseb_horjan_")]
+[assembly: AssemblyProduct("PG4500_2015_Innlevering1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
