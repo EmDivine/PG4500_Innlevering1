c8d8ac5996e621e88b56df06f8dbbafc05d5aea6
Zebezd <zebbom@hotmail.com>
2015-03-10 13:45:29 +0100

    added constructors; added robocode.xml

--- PG4500_2015_Innlevering1/malseb_horjan_Robot.cs
+++ PG4500_2015_Innlevering1/malseb_horjan_Robot.cs
@@ -5,6 +5,18 @@ namespace PG4500_2015_Innlevering1
 {
 	public class malseb_horjan_Robot : AdvancedRobot
 	{
+
+		private Gunner gunner;
+		private Scout scout;
+		private Driver driver;
+
+		public malseb_horjan_Robot()
+		{
+			gunner = new Gunner(this);
+			scout = new Scout(this);
+			driver = new Driver(this);
+		}
+
 		public override void Run()
 		{
 
